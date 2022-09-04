3c9929bd002229917246d1c9ca16fa2e15873b91
Milind Gupta <62803826+MilindGupta-Creator@users.noreply.github.com>
2022-09-04 16:51:47 +0530

    Time: 202 ms (14.93%), Space: 46.7 MB (80.31%) - LeetHub

---
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        
+        sort(nums.begin(),nums.end());
+        
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]==nums[i]){
+                return true;
+            }else{
+                continue;
+            }
+        }
+        
+        return false;
+        
+    }
+};
