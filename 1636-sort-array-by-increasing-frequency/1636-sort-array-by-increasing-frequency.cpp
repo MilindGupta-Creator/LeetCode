733a1fcef4ef636ecaad674c4eeef0e7505ffed9
Milind Gupta <62803826+MilindGupta-Creator@users.noreply.github.com>
2022-09-19 16:17:16 +0530

    Time: 13 ms (57.27%), Space: 11.3 MB (73.94%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -3,33 +3,33 @@ public:
     vector<int> frequencySort(vector<int>& nums) {
         unordered_map<int,int>mp;
         
-        for(auto it:nums){
+        for(auto it:nums)
+        {
             mp[it]++;
         }
         
-        priority_queue<pair<int,int>>pq;
+        priority_queue<pair<int,int>>pq; // freq,element
         
-        for(auto it:mp){
-            pq.push({-it.second,it.first});
-            //min heap
+        for(auto it:mp)
+        {
+            pq.push({-1*it.second,it.first});
         }
         
         vector<int>ans;
+        
         while(!pq.empty())
         {
             auto cnt = -1*(pq.top().first);
             
-            while(cnt--){
+            while(cnt--)
+            {
                 ans.push_back(pq.top().second);
             }
             
             pq.pop();
         }
         
+        
         return ans;
-        
-        
-            
-        
     }
 };
