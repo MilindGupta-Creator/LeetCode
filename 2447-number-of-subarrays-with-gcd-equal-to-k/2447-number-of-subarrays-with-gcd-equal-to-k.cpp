05ee345d6ac7b28a5d1eed52ed4ac4521d74536f
Milind Gupta <62803826+MilindGupta-Creator@users.noreply.github.com>
2022-11-12 23:29:02 +0530

    Time: 14 ms (95.55%), Space: 8.9 MB (75.56%) - LeetHub

--- 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
+++ 2447-number-of-subarrays-with-gcd-equal-to-k/2447-number-of-subarrays-with-gcd-equal-to-k.cpp
@@ -33,7 +33,10 @@ public:
                     ans++;
                 }
                 
-                
+                if(gcd<k)
+                {
+                    break;
+                }
                     
             }
         }
